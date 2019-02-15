31cf83ac2fa2b45a57975b2033ff5888581e2baa
alumno <pgmcastillo98@gmail.com>
2019-02-15 14:12:03 +0100

    xxxxxxx

--- index.php
+++ index.php
@@ -8,17 +8,32 @@ $provincias = $client->ObtenerProvincias();
 $provincias = simplexml_load_string($provincias->any);
 $provincias = $provincias->provinciero->prov;
 
-$desplegable = "<select name='prov'>";
+$desplegable = "<select name='prov' style='padding:2px; width:25%'>";
 foreach ($provincias as $prov) {
     $desplegable .= "<option value='$prov->np'> $prov->np</option>";
 }
-$desplegable .= "</select>";
+$desplegable .= "</select><br>";
 
 if ($_POST['enviar']) {
     $provincia = $_POST['prov'];
     $municipio = $client->ObtenerMunicipios($provincia);
     $municipio = simplexml_load_string($municipio->any);
     $municipio = $municipio->municipiero->muni;
+
+    $desplegable2 = "<select style='margin-top: 15px; padding:2px; width:25%' name='muni'>";
+    foreach ($municipio as $m) {
+        $desplegable2 .= "<option value='$m->nm'>$m->nm</option>";
+    }
+    $desplegable2 .= "</select><br/><input type='submit' style='margin-top: 5px;' name='enviar2' "
+            . "value='ENVIAR MUNICIPIO'>";
+    if (isset($_POST['enviar2'])) {
+        $muni = $_POST['muni'];
+        if ($muni === "CAMPILLO DE DUEÑAS") {
+            $txt = "Has seleccionado " . $muni . ", el mejor pueblo de todos, de $provincia";
+        } else {
+            $txt = "Has seleccionado " . $muni . ", de $provincia";
+        }
+    }
 }
 ?>
 <!doctype html>
@@ -30,13 +45,11 @@ if ($_POST['enviar']) {
     <body>
         <form action="." method="POST">
             <?php echo $desplegable ?>
-            <input type="submit" name="enviar" value="ENVIAR">
+            <input type="submit" style='margin-top: 5px;' name="enviar" value="ENVIAR PROVINCIA">
             <br/>
-            <?php
-            foreach ($municipio as $m) {
-                echo $m->nm . "<br>";
-            }
-            ?>
+            <?php echo $desplegable2; ?>
+            <?php echo $txt ?>
         </form>
+
     </body>
 </html>
